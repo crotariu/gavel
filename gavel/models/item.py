ed9c973d4fa1ee1ebdeb1ef87315ef5991ed998c
cosmin <crotariu@gmail.com>
2023-06-02 13:25:04 +0300

    Added support for patentable checkbox

--- gavel/models/item.py
+++ gavel/models/item.py
@@ -21,6 +21,7 @@ class Item(db.Model):
     active = db.Column(db.Boolean, default=True, nullable=False)
     viewed = db.relationship('Annotator', secondary=view_table)
     prioritized = db.Column(db.Boolean, default=False, nullable=False)
+    patentable = db.Column(db.Boolean, default=False, nullable=False)
 
     mu = db.Column(db.Float)
     sigma_sq = db.Column(db.Float)
